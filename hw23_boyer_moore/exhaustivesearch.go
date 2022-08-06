27ce41e203d9c135cb006ea892bfc659b1bf4fe6
Aleksey.Molokov <aleksey.molokov@x5.ru>
2022-08-06 18:06:27 +0300

    hw23: remove import utf8

--- hw23_boyer_moore/exhaustivesearch.go
+++ hw23_boyer_moore/exhaustivesearch.go
@@ -1,22 +1,21 @@
 package hw23
 
-import "unicode/utf8"
-
 const patternNotFound = -1
 
 type ExhaustiveSearch struct{}
 
 // Search поиск индекса в тексте с которого начинается шаблон или -1.
 func (s *ExhaustiveSearch) Search(text, pattern string) int {
-	lenText := utf8.RuneCountInString(text)
-	lenPattern := utf8.RuneCountInString(pattern)
+	textR := []rune(text)
+	patternR := []rune(pattern)
+
+	lenText := len(textR)
+	lenPattern := len(patternR)
 
 	if lenPattern > lenText {
 		return patternNotFound
 	}
 
-	textR := []rune(text)
-	patternR := []rune(pattern)
 	to := lenText - lenPattern
 
 	for i := 0; i <= to; i++ {
